dd690b3782affa48662659b588de1f3114e040a2
jaga-practice <129055774+jaga-practice@users.noreply.github.com>
2023-12-06 05:14:26 +0530

    Update delete-old-logs.sh

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 Directory=/c/Users/jagadeesh/desktop/logs
-Date=$(date+%F-%H-%M_%S)
+Date=$(date +"%F-%H-%M-%S")
 Log_file="$Date.log"
-Input=$(find /c/Users/jagadeesh/desktop/logs-name "*.log"-type f mtime +14)
+Input=$(find /c/Users/jagadeesh/desktop/logs -name "*.log" -type f mtime +14)
 while IFS=read -r line
 do 
 echo "Deleting logfile : $line" &>>Log_file
-rm -rf $line
+#rm -rf $line
 done <<< "$Input"
