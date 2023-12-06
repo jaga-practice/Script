c25128bdf4a3759df9825ca42a004b576bed469e
jaga-practice <129055774+jaga-practice@users.noreply.github.com>
2023-12-06 05:38:20 +0530

    Update delete-old-logs.sh

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -2,8 +2,8 @@
 Directory=/home/centos/logs
 Date=$(date +"%F-%H-%M-%S")
 Log_file="$Date.log"
-Input=$(find $Directory -name "*.log" -type f mtime +14)
-while IFS=read line
+Input=$(find $Directory -name "*.log" -type f mtime+14)
+while IFS= read -r line
 do 
 echo "Deleting logfile : $line" &>>Log_file
 #rm -rf $line
